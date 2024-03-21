99ebf17960da1ad737eb611d1a8ed6cb03301466
Fedecavazzoli <fedecavazzoli@gmail.com>
2024-03-21 17:27:32 -0300

    prueba commit

--- Consultorio/bookings/urls.py
+++ Consultorio/bookings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 def mi_vista(request):
-    return HttpResponse("<h3>Bienvenidos a la aplicacion de reservas</h3>")
+    return HttpResponse("<h3>Bienvenidos a la aplicacion de reservas!!!!</h3>")
 
 urlpatterns = [
     path("", mi_vista)
