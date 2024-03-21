9d97265aa71e8975243ea4ea02fc9d33804e341c
Fedecavazzoli <fedecavazzoli@gmail.com>
2024-03-21 16:46:46 -0300

    bookings urls

---
+++ Consultorio/bookings/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def mi_vista(request):
+    return HttpResponse("<h3>Bienvenidos a la aplicacion de reservas</h3>")
+
+urlpatterns = [
+    path("", mi_vista)
+]
